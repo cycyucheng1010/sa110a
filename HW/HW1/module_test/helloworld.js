67f1646cc9520976b3b9afbb3f09e58718f2f6fc
Rick Chen <rick20001010@gmail.com>
2021-09-25 17:50:41 +0800

    20210925

--- HW/HW1/module_test/helloworld.js
+++ HW/HW1/module_test/helloworld.js
@@ -1,3 +1,4 @@
 import * as _ from '../cychelloworld.js'
-var x=1
+import * as p from 'https://deno.land/x/math@v1.1.0/pow.ts'
+var x= p.pow(2,9)
 console.log('hello world',_.helloworld(x))
---
+++ HW/HW1/module_test/test3.js
@@ -0,0 +1,17 @@
+import * as _ from "../cycafter.js";
+import{assert} from "https://deno.land/std@0.108.0/testing/asserts.ts"
+
+Deno.test("findindex",()=>{
+var saves = ['profile', 'settings'];
+ 
+var done = _.after(saves.length, function() {
+  console.log('done saving!');
+});
+/*
+_.forEach(saves, function(type) {
+  asyncSave({ 'type': type, 'complete': done });
+});
+// => Logs 'done saving!' after the two async saves have completed.
+*/
+})
+
